5342b0bb3b6214350c452e884a329f2706f08071
andrejristevski <andrej.rist@gmail.com>
2018-03-07 16:32:51 +0100

    Timeout not introduced

--- dataDownloader.js
+++ dataDownloader.js
@@ -27,13 +27,17 @@ const getDataFromRest = async (url) => {
         uri: url,
         json: true
     };
-    let dayDataBody = await rp(options)
-    return dayDataBody;
+    try {
+        let dayDataBody = await rp(options)
+        return dayDataBody;
+    } catch (e) {
+        console.log(`Error downloading data for ${url} with error ${e}`);
+    }
+    return null;
 }
 
 const downloadMissingDataForCurrency = async (currencyConfig) => {
 
-    // get lates date for currency
     let latestDownloadedForCcy = await dataService.getLatestDownloadedForCcy(currencyConfig);
 
     let endDate = new Date();
@@ -44,25 +48,40 @@ const downloadMissingDataForCurrency = async (currencyConfig) => {
         return;
     }
 
-    let datesToBeDownloaded = getDatesBetween(latestDownloadedForCcy, endDate)
+    let datesToBeDownloaded = getDatesBetween(latestDownloadedForCcy, endDate);
 
     for (date of datesToBeDownloaded) {
 
         let url = getUrlForDate(date, currencyConfig.currency)
-        let curencyData = await getDataFromRest(url)
-        curencyData.exactDate = date;
-        curencyData.exactDateStr = shortIsoStringFromDate(date);
+        let curencyData = await getDataFromRest(url);
+        if (curencyData) {
 
-        await dataService.saveDayData(currencyConfig, curencyData)
-        let breakpoint = 0;
+            curencyData.exactDate = date;
+            curencyData.exactDateStr = shortIsoStringFromDate(date);
+            await dataService.saveDayData(currencyConfig, curencyData)
+
+        } else {
+
+            setTimeout(() => {
+                downloadMissingDataForCurrency(currencyConfig);
+                console.log(`Starting again and breaking once`);
+            }, 1000);
+            return
+        }
     }
 }
 
 const downloadMissingData = async () => {
+    let startTime = new Date();
     for (let curencyConfig of config.currencies) {
-        let missingDataForCurrency = await downloadMissingDataForCurrency(curencyConfig);
-        let breakpoint = 0;
+        let startTimeCurrency = new Date();
+        await downloadMissingDataForCurrency(curencyConfig);
+        let endTimeCurrency = new Date();
+        console.log(```For currency ${curencyConfig.currency} it took
+         ${(endTimeCurrency - startTimeCurrency) / 1000} seconds ```);
     }
+    console.log(```For all currencies it took
+         ${(new Date() - startTimeCurrency) / 1000} seconds ```);
 }
 
 const startDownloadingInterval = () => {
