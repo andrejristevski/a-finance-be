cd839f913863e07a72b2196810233522486733c8
andrejristevski <andrej.rist@gmail.com>
2018-03-24 01:13:54 +0100

    Added port

--- server/server.js
+++ server/server.js
@@ -14,6 +14,6 @@ app.use(function(req, res, next) {
 
 app.use('/', routes);
 
-app.listen(5000, function () {
+app.listen(process.env.PORT || 5000, function () {
     console.log('Example app listening on port 5000!')
 });
