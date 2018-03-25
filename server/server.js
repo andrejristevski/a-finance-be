a85b95028f6d23239250de9755db44ad6c3fadb3
andrejristevski <andrej.rist@gmail.com>
2018-03-26 00:02:47 +0200

    Empty change

--- server/server.js
+++ server/server.js
@@ -5,7 +5,6 @@ const routes = require('./routes')
 const app = express()
 app.use(bodyParser.json())
 
-
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
